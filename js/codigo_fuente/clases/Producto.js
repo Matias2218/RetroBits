7d9bb1582b37e65ee32265b8e767cc250f7a994b
Matias <maldo1514@gmail.com>
2019-11-17 13:03:27 -0300

    Initial commit

---
+++ js/codigo_fuente/clases/Producto.js
@@ -0,0 +1,9 @@
+class Producto {
+    constructor(id, nombre, tipo, descripcion, imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.imagen = imagen;
+    }
+}
