88a9a8e18433dc2e1ec5e6a4d15565780fad779a
Matias <maldo1514@gmail.com>
2019-11-18 17:23:42 -0300

    ahora funciona logout

--- js/codigo_fuente/servicios/UsuarioServicio.js
+++ js/codigo_fuente/servicios/UsuarioServicio.js
@@ -1,21 +1,68 @@
 import {Usuario} from '../clases/Usuario.js';
 
+document.addEventListener('DOMContentLoaded', ()=> {
+    const divsIniciarCrearCuenta = "\n" +
+        "        <ul class=\"navbar-nav mr-auto\">\n" +
+        "            <li class=\"nav-item \">\n" +
+        "                <a class=\"nav-link\" href=\"#\">Inicio</a>\n" +
+        "            </li>\n" +
+        "            <li class=\"nav-item\">\n" +
+        "                <a class=\"nav-link\" href=\"productos.html\">Productos</a>\n" +
+        "            </li>\n" +
+        "            <li class=\"nav-item\">\n" +
+        "                <a class=\"nav-link\" href=\"#\">Galeria</a>\n" +
+        "            </li>\n" +
+        "        </ul>\n" +
+        "        <div style=\"height: 40px; padding: 6px 0 6px;\"><a data-toggle=\"modal\" id=\"btnIniciarSesion\" data-target=\"#modalLRForm\" class=\"text-dark p-6\">Iniciar\n" +
+        "            Sesion</a></div>\n" +
+        "        <div class=\"text-center\">\n" +
+        "            <a href=\"\" class=\"btn btn-dark btn-rounded\" id=\"btnCrearCuenta\" data-toggle=\"modal\"\n" +
+        "               data-target=\"#modalLRForm\">\n" +
+        "                Crear Cuenta</a>\n" +
+        "        </div>";
+    if(sessionStorage.getItem('usuarioLogeado') === null)
+    {
+        document.getElementById('basicExampleNav').innerHTML =   divsIniciarCrearCuenta;
+        document.getElementById('btnCrearCuenta').addEventListener('click', ()=>{
+            activarIniciarSesion();
+        });
 
-document.getElementById('btnCrearCuenta').addEventListener('click', ()=>{
-    activarIniciarSesion();
+        document.getElementById('btnIniciarSesion').addEventListener('click', ()=>{
+            activarCrearCuenta();
+        });
+
+        document.getElementById('iniciaSesionHref').addEventListener('click', ()=>{
+            activarCrearCuenta();
+        });
+
+        document.getElementById('creaCuentaHref').addEventListener('click', ()=>{
+            activarIniciarSesion();
+        });
+    }
+    else {
+        const nombre = JSON.parse(sessionStorage.getItem('usuarioLogeado')).nombre;
+        const datos = `<ul class="navbar-nav mr-auto">
+            <li class="nav-item ">
+                <a class="nav-link" href="#">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="productos.html">Productos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Galeria</a>
+            </li>
+        </ul> 
+        <h4 style="margin-right: 25px">Bienvenido ${nombre}</h4>
+        <a class="nav link" id="btnCerrarSesion" href="#">Cerrar sesión</a>`;
+        document.getElementById('basicExampleNav').innerHTML = datos;
+
+        document.getElementById("btnCerrarSesion").addEventListener("click", ()=>{
+           sessionStorage.removeItem('usuarioLogeado');
+           window.location.href = "index.html";
+        });
+    }
 });
 
-document.getElementById('btnIniciarSesion').addEventListener('click', ()=>{
-  activarCrearCuenta();
-});
-
-document.getElementById('iniciaSesionHref').addEventListener('click', ()=>{
-    activarCrearCuenta();
-});
-
-document.getElementById('creaCuentaHref').addEventListener('click', ()=>{
-    activarIniciarSesion();
-});
 
 $('#modalLRForm').on('hidden.bs.modal', ()=>{
     const formRegistro = document.getElementById('formRegistro');
@@ -55,6 +102,8 @@ document.getElementById('btnIniciarSesionUsuario').addEventListener('click', ()=
    });
    console.log('No puede logear');
 });
+
+
 
 (function() {
     'use strict';
